54158d8c6c65d5ddc9ea477ce8e3e58113475c15
eighteen-k-gold-malow <46029824+eighteen-k-gold-malow@users.noreply.github.com>
2025-10-31 22:53:44 +0800

    update

--- TCPClient/TCPClient.gd
+++ TCPClient/TCPClient.gd
@@ -1,9 +1,10 @@
-
+extends Node
 class_name TCPClient
 
-signal on_connect(sender:TCPClient)
-signal on_disconnect(sender:TCPClient)
-signal on_message(sender:TCPClient, data:PackedByteArray)
+signal connected(sender:TCPClient)
+signal disconnected(sender:TCPClient)
+signal message(sender:TCPClient, data:PackedByteArray)
+signal status_changed(sender:TCPClient, status:Status)
 
 enum Status {
 	STATUS_CONNECTED,
@@ -14,59 +15,67 @@ enum Status {
 
 var _tcp:StreamPeerTCP = null
 var _tls:StreamPeerTLS = null
-var _is_use_tls:bool = false
-var _status:Status = Status.STATUS_DISCONNECTED
-var _tls_hostname:String = ""
+var status:Status = Status.STATUS_DISCONNECTED
+
+var use_tls:bool = false
+var hostname:String = ""
+var timeout_second: float = 30.0
+var _connect_duration: float = 0
 
 func _init() -> void:
 	_tcp = StreamPeerTCP.new()
 	_tls = StreamPeerTLS.new()
 
-func connect_to_host(address:String, port:int,use_tls:bool=false, tls_hostname:String="") -> Error:
-	if _status != Status.STATUS_DISCONNECTED:
+func _exit_tree() -> void:
+	_disconnect_from_host(true)
+
+func _process(delta: float) -> void:
+	_poll()
+	match status:
+		Status.STATUS_CONNECTING, Status.STATUS_HANDSHAKEING:
+			_connect_duration += delta
+			if _connect_duration > timeout_second:
+				_disconnect_from_host(true)
+		_:
+			_connect_duration = 0
+
+func connect_to_host(address:String, port:int) -> Error:
+	if status != Status.STATUS_DISCONNECTED:
 		return Error.ERR_BUSY
-	_status = Status.STATUS_CONNECTING
-	_is_use_tls = use_tls
-	_tls_hostname = tls_hostname
+	status = Status.STATUS_CONNECTING
+	status_changed.emit(self, status)
 	var err:Error = _tcp.connect_to_host(address, port)
 	if err != Error.OK:
-		_status = Status.STATUS_DISCONNECTED
+		status = Status.STATUS_DISCONNECTED
+		status_changed.emit(self, status)
 	return err
 
 func _disconnect_from_host(is_poll:bool=true) -> void:
-	if _tls.get_status() != _tls.STATUS_DISCONNECTED:
-		_tls.disconnect_from_stream()
-	if _tcp.get_status() != _tcp.STATUS_NONE:
-		_tcp.disconnect_from_host()
-	if is_poll:
-		poll()
+	if status != Status.STATUS_DISCONNECTED:
+		if _tls.get_status() != _tls.STATUS_DISCONNECTED:
+			_tls.disconnect_from_stream()
+		if _tcp.get_status() != _tcp.STATUS_NONE:
+			_tcp.disconnect_from_host()
+		if is_poll:
+			_poll()
 
 func disconnect_from_host() -> void:
 	_disconnect_from_host(true)
 
-func get_status() -> Status:
-	return _status
-
-func is_use_tls() -> bool:
-	return _is_use_tls
-
-func get_tls_hostname() -> String:
-	return _tls_hostname
-
 func put_data(data:PackedByteArray) -> Error:
-	if _is_use_tls:
+	if use_tls:
 		return _tls.put_data(data)
 	else:
 		return _tcp.put_data(data)
 
-func poll() -> void:
-	match _status:
+func _poll() -> void:
+	match status:
 		Status.STATUS_CONNECTING:
 			_tcp_connect_func()
 		Status.STATUS_HANDSHAKEING:
 			_tls_connect_func()
 		Status.STATUS_CONNECTED:
-			if _is_use_tls:
+			if use_tls:
 				_tls_receive_func()
 			else:
 				_tcp_receive_func()
@@ -76,42 +85,50 @@ func poll() -> void:
 func _tcp_connect_func() -> void:
 	_tcp.poll()
 	match _tcp.get_status():
+		_tcp.STATUS_CONNECTING:
+			return
 		_tcp.STATUS_CONNECTED:
-			if _is_use_tls:
-				_status = Status.STATUS_HANDSHAKEING
-				if _tls.connect_to_stream(_tcp, _tls_hostname) != Error.OK:
-					_status = Status.STATUS_DISCONNECTED
-					on_disconnect.emit(self)
+			if use_tls:
+				status = Status.STATUS_HANDSHAKEING
+				status_changed.emit(self, status)
+				if _tls.connect_to_stream(_tcp, hostname) != Error.OK:
+					status = Status.STATUS_DISCONNECTED
+					status_changed.emit(self, status)
+					disconnected.emit(self)
 					return
 			else:
-				_status = Status.STATUS_CONNECTED
-				on_connect.emit(self)
-			return
-		_tcp.STATUS_ERROR:
-			_status = Status.STATUS_DISCONNECTED
-			on_disconnect.emit(self)
+				status = Status.STATUS_CONNECTED
+				status_changed.emit(self, status)
+				connected.emit(self)
 			return
 		_:
-			pass
+			status = Status.STATUS_DISCONNECTED
+			status_changed.emit(self, status)
+			disconnected.emit(self)
+			return
 
 func _tls_connect_func() -> void:
 	_tls.poll()
 	match _tls.get_status():
 		_tls.STATUS_CONNECTED:
-			_status = Status.STATUS_CONNECTED
-			on_connect.emit(self)
+			status = Status.STATUS_CONNECTED
+			status_changed.emit(self, status)
+			connected.emit(self)
 			return
 		_tls.STATUS_HANDSHAKING:
 			return
 		_:
-			_status = Status.STATUS_DISCONNECTED
-			on_disconnect.emit(self)
+			status = Status.STATUS_DISCONNECTED
+			status_changed.emit(self, status)
+			disconnected.emit(self)
+			return
 
 func _tcp_receive_func() -> void:
-	while _status == Status.STATUS_CONNECTED:
+	while status == Status.STATUS_CONNECTED:
 		if not(_tcp.poll() == Error.OK and _tcp.get_status() == _tcp.STATUS_CONNECTED):
-			_status = Status.STATUS_DISCONNECTED
-			on_disconnect.emit(self)
+			status = Status.STATUS_DISCONNECTED
+			status_changed.emit(self, status)
+			disconnected.emit(self)
 			return
 		var bytes_count:int = _tcp.get_available_bytes()
 		if bytes_count > 0:
@@ -119,16 +136,18 @@ func _tcp_receive_func() -> void:
 			var err:Error = result[0] as Error
 			var data:PackedByteArray = result[1] as PackedByteArray
 			if err == Error.OK:
-				on_message.emit(self, data)
+				message.emit(self, data)
+			continue
 		else:
 			return
 
 func _tls_receive_func() -> void:
-	while _status == Status.STATUS_CONNECTED:
+	while status == Status.STATUS_CONNECTED:
 		_tls.poll()
 		if not(_tls.get_status() == _tls.STATUS_CONNECTED):
-			_status = Status.STATUS_DISCONNECTED
-			on_disconnect.emit(self)
+			status = Status.STATUS_DISCONNECTED
+			status_changed.emit(self, status)
+			disconnected.emit(self)
 			return
 		var bytes_count:int = _tls.get_available_bytes()
 		if bytes_count > 0:
@@ -136,6 +155,7 @@ func _tls_receive_func() -> void:
 			var err:Error = result[0] as Error
 			var data:PackedByteArray = result[1] as PackedByteArray
 			if err == Error.OK:
-				on_message.emit(self, data)
+				message.emit(self, data)
+			continue
 		else:
 			return
--- readme.md
+++ readme.md
@@ -4,33 +4,29 @@
 ```gds
 extends Control
 
-@onready var tcp_client:TCPClient = TCPClient.new()
+@onready var tcp_client: TCPClient = TCPClient.new()
 
-func _ready() -> void:
-	tcp_client.on_connect.connect(on_connect)
-	tcp_client.on_disconnect.connect(on_disconnect)
-	tcp_client.on_message.connect(on_message)
+func on_connected(_sender) -> void:
+	print("connect")
+	tcp_client.put_data("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n".to_utf8_buffer())
 
-    # tcp_client.disconnect_from_host()
-	if tcp_client.connect_to_host("baidu.com", 443, true, "baidu.com") == Error.OK:
+func on_disconnected(_sender) -> void:
+	print("disconnect")
+
+func on_message(_sender, data:PackedByteArray) -> void:
+	print("message: %s\r\n" % data.get_string_from_utf8())
+
+func _ready():
+	add_child(tcp_client)
+	tcp_client.connected.connect(on_connected)
+	tcp_client.disconnected.connect(on_disconnected)
+	tcp_client.message.connect(on_message)
+	tcp_client.timeout_second = 30.0
+	tcp_client.use_tls = true
+	tcp_client.hostname = "baidu.com"
+	if tcp_client.connect_to_host("baidu.com", 443) == Error.OK:
 		print("connect_to_host ok")
 	else:
 		print("connect_to_host fail")
-
-func _exit_tree() -> void:
-	tcp_client.disconnect_from_host()
-
-func _process(delta: float) -> void:
-	tcp_client.poll()
-
-func on_connect(sender:TCPClient) -> void:
-	print("on_connect")
-	sender.put_data("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n".to_utf8_buffer())
-
-func on_disconnect(sender:TCPClient) -> void:
-	print("on_disconnect")
-
-func on_message(sender:TCPClient, data:PackedByteArray) -> void:
-	print("on_message: %s" % data.get_string_from_utf8())
 
 ```
